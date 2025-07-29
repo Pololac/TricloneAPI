cc0f80e9e20e12957799986a4f408ac3994cb34c
Pololac <lacostepaul@yahoo.fr>
2025-07-29 16:19:44 +0200

    Commentaires ajoutés sur la SecurityFilterChain

--- src/main/java/com/hb/cda/examapi/security/SecurityConfig.java
+++ src/main/java/com/hb/cda/examapi/security/SecurityConfig.java
@@ -15,13 +15,30 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain accessControl (HttpSecurity http) throws Exception {
         http
-            .httpBasic(Customizer.withDefaults())
-            .csrf((csrf) -> csrf.disable())
-            .authorizeHttpRequests((req) -> req
-            .requestMatchers("/api/accounts").authenticated()
-            .anyRequest().permitAll()
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    // Authentification requise pour /api/accounts et tout ce qui commence par /api/accounts/
+                    .requestMatchers("/api/accounts", "/api/accounts/**").authenticated()
+                    // Tout le reste reste public
+                    .anyRequest().permitAll()
             )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+            // Ici tu peux choisir IF_REQUIRED ou STATELESS selon ton besoin
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            )
+            // Méthode d’authentification "Basic" : pas via /api/login mais en rajoutant
+            // un header "Authorization" -> "Basic Auth" (username+MdP) dans les requêtes Postman
+            .httpBasic(Customizer.withDefaults()
+
+            /* Si on passe par un formulaire, mais pas recommandé sur API Rest
+            .formLogin(form -> form
+                    .loginProcessingUrl("/api/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .successHandler((req,res,auth)->res.setStatus(HttpServletResponse.SC_OK))
+                    .failureHandler((req,res,e)->res.setStatus(HttpServletResponse.SC_UNAUTHORIZED))
+             */
+            );
         return http.build();
     }
 
